86a0ae9488542e92b06f3caf22df993af616053e
PelleChristensen <pelle.christensen@gmail.com>
2025-02-13 20:31:43 +0100

    opdatering

---
+++ XR-SPL2/Assets/scripts/Environment.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class Environment : MonoBehaviour
+{
+
+    void Awake()
+    {
+        Debug.Log("Environment has awakened"); 
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
